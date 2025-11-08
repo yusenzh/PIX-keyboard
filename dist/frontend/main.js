9e496ace0145b6c9731b085c660324aaf5bc5585
Yusen Zhang <yusenzhang65@hotmail.com>
2025-11-08 12:35:04 -0500

    Added handlers for 9 buttons

--- dist/frontend/main.js
+++ dist/frontend/main.js
@@ -250,38 +250,37 @@ __webpack_require__.r(__webpack_exports__);
 
 var engine = new _engine__WEBPACK_IMPORTED_MODULE_0__.Engine();
 // This function will be run when the page loads. It will handle making new elements, attaching event listeners, etc. 
+// We are going to build a calculator 
 window.onload = function () {
     var canvas = document.getElementById("gameCanvas");
     var draw = canvas.getContext("2d");
-    // because this function is defined here (inside the window.onload function), it has access to the canvas and draw variables.
-    var newFrameWithRectangle = function (rectangleSize, rectangleColor) {
-        // empty the screen
-        draw.clearRect(0, 0, canvas.width, canvas.height);
-        // set the color
-        draw.fillStyle = rectangleColor;
-        // draw a rectangle centered on the canvas
-        draw.fillRect((canvas.width - rectangleSize) / 2, (canvas.height - rectangleSize) / 2, rectangleSize, rectangleSize);
-    };
     document.addEventListener("keydown", function (event) {
         if (event.code == "Digit1") {
             console.log("1!");
-            newFrameWithRectangle(10, "#7400b8");
         }
         else if (event.code == "Digit2") {
             console.log("2!");
-            newFrameWithRectangle(20, "#5390d9");
         }
         else if (event.code == "Digit3") {
             console.log("3!");
-            newFrameWithRectangle(30, "#48bfe3");
         }
         else if (event.code == "Digit4") {
             console.log("4!");
-            newFrameWithRectangle(40, "#64dfdf");
         }
         else if (event.code == "Digit5") {
             console.log("5!");
-            newFrameWithRectangle(50, "#80ffdb");
+        }
+        else if (event.code == "Digit6") {
+            console.log("6!");
+        }
+        else if (event.code == "Digit7") {
+            console.log("7!");
+        }
+        else if (event.code == "Digit8") {
+            console.log("8!");
+        }
+        else if (event.code == "Digit9") {
+            console.log("9!");
         }
     });
 };
--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -2,44 +2,41 @@ import { Engine } from "./engine";
 let engine : Engine = new Engine();
 
 // This function will be run when the page loads. It will handle making new elements, attaching event listeners, etc. 
+// We are going to build a calculator 
 window.onload = () => {
 	let canvas : HTMLCanvasElement = document.getElementById("gameCanvas") as HTMLCanvasElement ;
 	
 	let draw : CanvasRenderingContext2D = canvas.getContext("2d");
 
-	// because this function is defined here (inside the window.onload function), it has access to the canvas and draw variables.
-	let newFrameWithRectangle = (rectangleSize: number, rectangleColor: string) : void => {
 
-		// empty the screen
-		draw.clearRect(0,0, canvas.width, canvas.height);
-
-		// set the color
-		draw.fillStyle = rectangleColor;
-
-		// draw a rectangle centered on the canvas
-		draw.fillRect((canvas.width - rectangleSize)/2,(canvas.height - rectangleSize)/2,rectangleSize,rectangleSize);
-	}
 
 	document.addEventListener("keydown", (event: KeyboardEvent) => { 
 		if (event.code == "Digit1") {
 			console.log("1!");
-			newFrameWithRectangle(10, "#7400b8");
 		}
 		else if (event.code == "Digit2") {
 			console.log("2!");
-			newFrameWithRectangle(20, "#5390d9");
 		}
 		else if (event.code == "Digit3") {
 			console.log("3!");
-			newFrameWithRectangle(30, "#48bfe3");
 		}
 		else if (event.code == "Digit4") {
 			console.log("4!");
-			newFrameWithRectangle(40, "#64dfdf");
 		}
 		else if (event.code == "Digit5") {
 			console.log("5!");
-			newFrameWithRectangle(50, "#80ffdb");
+		}
+		else if (event.code == "Digit6") {
+			console.log("6!");
+		}
+		else if (event.code == "Digit7") {
+			console.log("7!");
+		}
+		else if (event.code == "Digit8") {
+			console.log("8!");
+		}
+		else if (event.code == "Digit9") {
+			console.log("9!");
 		}
 	});
 }
